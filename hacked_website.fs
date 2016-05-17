69b5f3f26608ccc71a00b37737d7be9b8000e41f
Matthew Chan <matt@themattchan.com>
2016-05-16 19:04:37 -0700

    yay overflows

--- hacked_website.fs
+++ hacked_website.fs
@@ -3,6 +3,8 @@
 
 How many links?
 
+n choose 2, i.e. n!/[2! (n-2)!]
+
 Do we include self references in the link breaking?
 
 for each link, 0.5 chance that it is broken
@@ -11,4 +13,31 @@ then P(none broken) = 0.5 ^ #links
 
 *)
 
+let ncr n k =
+  let rec aux a n k =
+    if k = 0 then a
+    else aux ((n * a)/k) (n-1) (k-1)
+  in go 1 n k
+
+(*
+(1/2^k)
+
+10^15 = 2^x
+
+where x = 15(log2+log5)/log2
+
+so ==> 2^(x-k)
+*)
+(*
+let exp_x =
+  let log x = Math.Log((float x), 10.) in
+  (15. * (log 2 + log 5)) / (log 2)
+
 let Hacked_Website n =
+  2 ** (exp_x - (float (ncr n 2))) |> sprintf "%f"
+*)
+
+let big_num = pown 10 15
+
+let Hacked_Website n =
+  big_num / (ncr n 2) |> sprintf "%d"
