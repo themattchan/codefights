a76bb487b535317fb8ddd6e4608ecd86027e3ae9
Matthew Chan <matt@themattchan.com>
2016-05-16 18:24:57 -0700

    flerb

---
+++ hacked_website.fs
@@ -0,0 +1,14 @@
+(*
+ a website has n pages. for any two pages there exists a link.
+
+How many links?
+
+Do we include self references in the link breaking?
+
+for each link, 0.5 chance that it is broken
+
+then P(none broken) = 0.5 ^ #links
+
+*)
+
+let Hacked_Website n =
