8323ce84bca7ac0bfe4d9cf36c7b9cd2b13fa5cf
Matthew Chan <matt@themattchan.com>
2016-05-20 01:22:39 -0700

    holy shit their testcases used commas instead of semicolons

--- jet_categories.fs
+++ jet_categories.fs
@@ -1,25 +1,24 @@
 module jet
 open System
 
-let rec go_insert things more =
-    match (things,more) with
-    | ([], rest) -> rest
-    | things, [] -> things
-    | (t::ts), (m::ms) -> if t > m then (m :: t :: go_insert ts ms)
-                              else (t :: go_insert ts more)
-
-let rec go catalog updates newcat =
-    match (catalog,updates) with
-    | [],_ -> List.rev newcat
-    | log,[] -> List.rev (log@newcat)
-    | ((k1::v1)::logs), ((k2::v2)::ups) ->
-        if k1 = k2 then go logs ups ((k2::(go_insert v1 v2)) :: newcat)
-            else if k1 < k2 then go logs updates ((k1::v1) :: newcat)
-            else (* k1 > k2 *)   go catalog ups ((k2::v2) :: newcat)
-
 let catalogUpdate catalog updates =
     let updates' = List.sortBy List.head updates in
-    go catalog updates' []
+    let rec go_insert things more =
+        match things,more with
+            | [], rest -> rest
+            | things, [] -> things
+            | (t::ts), (m::ms) -> if t > m then (m :: t :: go_insert ts ms)
+                                      else (t :: go_insert ts more)
+    in
+    let rec go catalog updates newcat =
+        match catalog,updates with
+            | [],_ -> List.rev newcat
+            | log,[] -> List.rev (log@newcat)
+            | ((k1::v1)::logs), ((k2::v2)::ups) ->
+                if k1 = k2 then go logs ups ((k2::(go_insert v1 v2)) :: newcat)
+                    else if k1 < k2 then go logs updates ((k1::v1) :: newcat)
+                    else (* k1 > k2 *)   go catalog ups ((k2::v2) :: newcat)
+    in go catalog updates' []
 
 let catalog1 = [["Books";"Classics";"Fiction"];
                 ["Electronics";"Cell Phones";"Computers";"Ultimate item"];
