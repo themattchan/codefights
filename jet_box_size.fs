3a49dde28d5597987eed3e8141801134dc39522e
Matthew Chan <matt@themattchan.com>
2016-05-26 04:05:13 -0700

    flub

--- jet_box_size.fs
+++ jet_box_size.fs
@@ -3,17 +3,17 @@ let packageBoxing package boxes =
     let pack = List.sort package in
     let smaller p b = vol p < vol b in
     let fits b = List.zip pack b |>
-		List.forall (fun (x,y) -> x <= y)
-	in
+        List.forall (fun (x,y) -> x <= y)
+    in
     let fold_f (r, i, ob) b =
         let box = List.sort b in
-		let isSmaller = ob
-			|> Option.map (smaller box)
-			|> Option.getOrElse true
-		in
-		if fits box && isSmaller
-		then (i, i+1, Some box)
-		else (r, i+1, ob)
-	in
+        let isSmaller = ob
+            |> Option.map (smaller box)
+            |> Option.getOrElse true
+        in
+        if fits box && isSmaller
+        then (i, i+1, Some box)
+        else (r, i+1, ob)
+    in
     let (r,_,_) = List.fold fold_f ((-1), 0, None) boxes in
     r
