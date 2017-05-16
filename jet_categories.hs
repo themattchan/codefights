da2c1465429f8272843988c73f10a3581f95f26e
Matthew Chan <matt@themattchan.com>
2017-05-16 01:13:25 -0700

    ugh what's failing

--- jet_categories.hs
+++ jet_categories.hs
@@ -3,10 +3,9 @@ import Data.List
 import qualified Data.Map.Lazy as M
 
 catalogUpdate catalog updates =
- ff $ a ++ b ++ [r]
+  map (uncurry (:)) (a ++ b ++ [r])
   where
     (a,r:b)  = break ((=="root") . fst) updated
     updated  = M.toList . M.map (sort . nub)
              $ M.unionWith (++) (tomap updates) (tomap catalog)
-    tomap    = M.fromListWith (++) .  map (head &&& tail)
-    ff       = map (\(x,xs) -> x:xs)
+    tomap    = M.fromListWith (++) . map (head &&& tail)
